9fc7d5117a6917098dbd81ba1f722e1a803a46f4
kudaiar <kudi.akimaliev@gmail.com>
2021-02-28 15:22:48 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function foo (){
+	let text = document.getElementById('text');
+	if (text.style.display =='none'){
+		text.style.display =='block';
+	} else{
+		text.style.display =='none';
+	}
+}
+
+
+
+
+
+
+
+
+function kudi (){
+	let kuflo = document.getElementById('kubro');
+	kuflo.value='';
+}
+
